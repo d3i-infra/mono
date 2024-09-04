4fb6a199d1502f27fc8fc1338095c7f27b28049a
trbKnl <ncdeschipper@gmail.com>
2024-09-04 09:40:35 +0200

    fixed azure storage endpoint_form.ex

--- core/systems/storage/azure/endpoint_form.ex
+++ core/systems/storage/azure/endpoint_form.ex
@@ -4,7 +4,7 @@ defmodule Systems.Storage.Azure.EndpointForm do
   @impl true
   def render(assigns) do
     ~H"""
-    <div id={"#{@id}_azure_endpoint_content"} phx-hook="LiveContent" data-show-errors={@show_errors}>
+    <div id={"#{@id}_azure_endpoint_content"} phx-hook="LiveContent">
       <.form id={"#{@id}_azure_endpoint_form"} :let={form} for={@changeset} phx-change="save" phx-target={@myself}>
         <.text_input form={form} field={:account_name} label_text={dgettext("eyra-storage", "azure.account_name.label")} />
         <.text_input form={form} field={:container} label_text={dgettext("eyra-storage", "azure.container.label")} />
